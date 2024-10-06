2ce2974f2c82c35d85ae15491b4d984a85d5ab7c
Cosheimil <a.varvus@ya.ru>
2024-10-06 18:12:32 +0300

    Практикум по Программированию на Python

---
+++ ПпП-24/devenv.nix
@@ -0,0 +1,19 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  packages = with pkgs; [
+    jupyter-all
+    python311Packages.tkinter
+  ];
+
+  languages.python = {
+    enable = true;
+    venv.enable = true;
+    venv.requirements = ''
+      pandas
+      numpy
+      # tkinter
+      tk
+    '';
+  };
+}
