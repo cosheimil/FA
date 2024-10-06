3090f594fa0bf79e5e53f682904fa37126417094
Cosheimil <a.varvus@ya.ru>
2024-10-06 11:28:07 +0300

    Эконометрика...

---
+++ Эконометрика-24/devenv.nix
@@ -0,0 +1,25 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  packages = (with pkgs; [
+    jupyter-all
+  ]) ++ (with pkgs.python311Packages; [
+    pandas
+    scikit-learn
+    scipy
+    matplotlib
+    seaborn
+  ]);
+
+  languages.python = {
+    enable = true;
+    venv.enable = true;
+    venv.requirements = ''
+      matplotlib
+      scikit-learn
+      pandas
+      scipy
+      seaborn
+    '';
+  };
+}
